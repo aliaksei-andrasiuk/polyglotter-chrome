4ae58b6a862f7ee6e56dbf55dfcb2c155378ba8c
Aliaksei Andrasiuk <aliaksei.andrasiuk.business@gmail.com>
2025-05-19 19:57:39 +0200

    change config value to prod version

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-    // API_URL: "https://poliglotter-backend.onrender.com"
-    API_URL: "http://localhost:8081",
+    API_URL: "https://poliglotter-backend.onrender.com"
+    // API_URL: "http://localhost:8081",
 };
